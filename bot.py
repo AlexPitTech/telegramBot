3ddbacabda6f3e72c898484f77dffa42e38738b4
Alex Pit Tech <alex.pit.tech@gmail.com>
2020-05-09 18:10:07 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,35 @@
+import telebot
+import random
+from telebot.types import Message
+
+BOT_TOKEN = 'BOT_TOKEN=1274265608:AAFWLFOLZKO6CkJuSrqEySu0lv46cuWwJBk'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+smiles = [
+    '👍',
+    '🏻',
+    '😱',
+    '😅',
+    '🙊',
+    '😙',
+    '🤓',
+    '😞',
+    '☹',
+    '☺',
+    '😋',
+]
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Привет')
+
+
+@bot.message_handler(func=lambda message: True)
+def upper(message: Message):
+    bot.reply_to(message, random.choice)
+
+
+bot.polling()
+
